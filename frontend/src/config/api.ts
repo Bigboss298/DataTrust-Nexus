ee183c184ad2f558ac01312caf3c7e0edb828f62
Bigboss298 <abdullahbisiriyu988@gmail.com>
2025-10-20 12:19:19 +0100

    fix: chnages back baseurl

--- frontend/src/config/api.ts
+++ frontend/src/config/api.ts
@@ -9,8 +9,8 @@
 // ============================================
 // CHANGE THIS URL TO SWITCH ENVIRONMENTS
 // ============================================
-// const BASE_URL ='https://datatrust-nexus-gng7e8f0encbdtcr.westeurope-01.azurewebsites.net';
-const BASE_URL ='https://localhost:7218';
+const BASE_URL ='https://datatrust-nexus-gng7e8f0encbdtcr.westeurope-01.azurewebsites.net';
+// const BASE_URL ='https://localhost:7218';
 
 // ============================================
 // Alternative URLs (uncomment to use):
